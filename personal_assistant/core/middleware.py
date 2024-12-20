03cb5de6c23b46ae8b218bc0e5b014906b88d09b
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-21 00:08:20 +0200

    Delete middleware.py

--- personal_assistant/core/middleware.py
+++
@@ -1,5 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class CustomHeaderMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        request.META['CUSTOM_HEADER'] = 'PersonalAssistant'
