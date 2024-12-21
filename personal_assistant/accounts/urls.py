bf60ae7d23a28a015b4fe20c0bad3390d053c90f
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-21 13:45:21 +0200

    Update urls.py

--- personal_assistant/accounts/urls.py
+++ personal_assistant/accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Главная страница
+    path('', views.index, name='index'),  # Главная страница для accounts http://127.0.0.1:8000/accounts/
+    path('login/', views.login_view, name='login'),#http://127.0.0.1:8000/accounts/login/
+    path('signup/', views.signup_view, name='signup'),#http://127.0.0.1:8000/accounts/signup/
 ]
