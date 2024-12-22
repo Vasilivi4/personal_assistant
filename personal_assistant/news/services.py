36b95d8b13247648d73fccb34a51360f078f42a9
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-22 20:47:56 +0200

    Update services.py

--- personal_assistant/news/services.py
+++ personal_assistant/news/services.py
@@ -1,14 +1,16 @@
+from django.shortcuts import render
 import requests
 import logging
+
+from news.models import NewsSummary
 
 class NewsService:
     def __init__(self, api_key, base_url="https://newsapi.org/v2"):
         self.api_key = api_key
         self.base_url = base_url
         self.logger = logging.getLogger(__name__)
-        logging.basicConfig(level=logging.INFO)  # Вы можете настроить уровень логирования
 
-    def fetch_news(self, category=None, country="us", page_size=10, language="en", from_date=None, to_date=None):
+    def fetch_news(self, category=None, country="us", page_size=15, language="en", from_date=None, to_date=None, user=None):
         url = f"{self.base_url}/top-headlines"
         params = {
             "apiKey": self.api_key,
@@ -28,7 +30,16 @@ class NewsService:
             response = requests.get(url, params=params)
             response.raise_for_status()  # Raise an HTTPError for bad responses
             articles = response.json().get("articles", [])
-            print(articles)  # Выводим статьи в консоль для проверки
+
+            # Сохраняем новости в базу данных
+            for article in articles:
+                NewsSummary.objects.create(
+                    user=user,  # Указываем текущего пользователя
+                    title=article['title'],
+                    content=article.get('description', ''),
+                    date=article['publishedAt'][:10]  # Публикационная дата, обрезаем до даты (без времени)
+                )
+
             return articles
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Error fetching news: {e}")
@@ -41,12 +52,15 @@ class NewsService:
         
         try:
             response = requests.get(url, params=params)
-            response.raise_for_status()  # Raise an HTTPError for bad responses
+            response.raise_for_status()
             return response.json().get("sources", [])
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Error fetching sources: {e}")
-            # Можно вернуть дефолтные значения или пустой список
+
             return [
                 {"name": "Source 1", "description": "Description 1"},
                 {"name": "Source 2", "description": "Description 2"},
             ]
+def news_list_view(request):
+    news = NewsSummary.objects.all().order_by('-date')  # Получаем все новости, отсортированные по дате
+    return render(request, 'news/news_list.html', {'news': news})
