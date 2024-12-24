51e8e3bfb4956d53421b26db790d37698401abdb
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-24 20:00:32 +0200

    Update urls.py

--- personal_assistant/news/urls.py
+++ personal_assistant/news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from personal_assistant.news import views
+from news import views
 
 
 app_name = "news"
@@ -7,4 +7,5 @@ app_name = "news"
 urlpatterns = [
     path("", views.news_list, name="news_list"),
     path("sources/", views.news_sources, name="news_sources"),
+    path('weather/', views.weather_widget_view, name='weather_widget'),
 ]
