1aa97df117909c384590dd4c1beee76b29eec90d
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-24 20:00:08 +0200

    Update urls.py

--- personal_assistant/core/urls.py
+++ personal_assistant/core/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.news_list, name='news_list'),  # главная страница, отображает новости
     path('news/', views.news_list, name='news_list'),  # страница с новостями
     path('news/sources/', views.news_sources, name='news_sources'),  # страница с источниками новостей
+    path('news/weather/', views.weather_widget_view, name='weather_widget'),  # страница с погодой
 ]
