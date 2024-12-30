41c845ccd9d831a882c36ec920840361036f1a8a
Nik Kor <3357381Nk@gmail.com>
2024-12-30 16:21:54 +0000

    small code style changes

--- personal_assistant/notes/models.py
+++ personal_assistant/notes/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
-#classes for notes and tags. Each note might have multiple tags
+# classes for notes and tags. Each note might have multiple tags
 class Note(models.Model):
     user_id = models.IntegerField()
     title = models.CharField(max_length=100)
     content = models.TextField()
     done = models.BooleanField(default=False)
-    tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
+
     def __str__(self):
         return self.title
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
