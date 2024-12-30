a75f19f6ef63531b037a544d20a1664277f60b21
Xeljndjhtw <165076532+Xeljndjhtw@users.noreply.github.com>
2024-12-30 19:41:37 +0100

    Add files via upload

--- personal_assistant/files/models.py
+++ personal_assistant/files/models.py
@@ -1,3 +1,22 @@
+# models.py
+
 from django.db import models
 
-# Create your models here.
+# Модель для зберігання інформації про файли користувачів
+class UserFile(models.Model):
+    CATEGORY_CHOICES = [
+        ('image', 'Зображення'),
+        ('document', 'Документ'),
+        ('video', 'Відео'),
+        ('other', 'Інше'),
+    ]
+    file = models.FileField(upload_to='uploads/')  # Поле для файлу
+    category = models.CharField(
+        max_length=10,
+        choices=CATEGORY_CHOICES,
+        default='other'
+    )  # Категорія файлу
+    uploaded_at = models.DateTimeField(auto_now_add=True)  # Дата завантаження
+
+    def __str__(self):
+        return self.file.name
