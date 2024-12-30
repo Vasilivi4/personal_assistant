da4301ed5dfd739c4ee3135f7f82727e6f221a14
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-30 21:39:43 +0200

    Update services.py

--- personal_assistant/news/services.py
+++ personal_assistant/news/services.py
@@ -1,28 +1,48 @@
-from django.shortcuts import render
-import requests
+"""Module providing a function printing python version."""
+
 import logging
-from news.models import NewsSummary
+import requests
+from django.shortcuts import render
 from django import forms
+from news.models import NewsSummary
+
 
 class CategoryForm(forms.Form):
+    """Class CategoryForm representing a person"""
+
     CATEGORY_CHOICES = [
-        ('business', 'Business'),
-        ('entertainment', 'Entertainment'),
-        ('general', 'General'),
-        ('health', 'Health'),
-        ('science', 'Science'),
-        ('sports', 'Sports'),
-        ('technology', 'Technology'),
+        ("business", "Business"),
+        ("entertainment", "Entertainment"),
+        ("general", "General"),
+        ("health", "Health"),
+        ("science", "Science"),
+        ("sports", "Sports"),
+        ("technology", "Technology"),
     ]
-    category = forms.ChoiceField(choices=CATEGORY_CHOICES, required=False, label="Category")
+    category = forms.ChoiceField(
+        choices=CATEGORY_CHOICES, required=False, label="Category"
+    )
+
 
 class NewsService:
+    """Class NewsService representing a person"""
+
     def __init__(self, api_key, base_url="https://newsapi.org/v2"):
         self.api_key = api_key
         self.base_url = base_url
         self.logger = logging.getLogger(__name__)
 
-    def fetch_news(self, category=None, country="us", page_size=15, language="en", from_date=None, to_date=None, user=None):
+    def fetch_news(
+        self,
+        category=None,
+        country="us",
+        page_size=15,
+        language="en",
+        from_date=None,
+        to_date=None,
+        user=None,
+    ):
+        """Function fetch_news printing python version."""
         url = f"{self.base_url}/top-headlines"
         params = {
             "apiKey": self.api_key,
@@ -40,16 +60,15 @@ class NewsService:
 
         try:
             response = requests.get(url, params=params)
-            response.raise_for_status()  # Raise an HTTPError for bad responses
+            response.raise_for_status()
             articles = response.json().get("articles", [])
 
-            # Сохраняем новости в базу данных
             for article in articles:
                 NewsSummary.objects.create(
-                    user=user,  # Указываем текущего пользователя
-                    title=article['title'],
-                    content=article.get('description', ''),
-                    date=article['publishedAt'][:10]  # Публикационная дата, обрезаем до даты (без времени)
+                    user=user,
+                    title=article["title"],
+                    content=article.get("description", ""),
+                    date=article["publishedAt"][:10],
                 )
 
             return articles
@@ -57,11 +76,11 @@ class NewsService:
             self.logger.error(f"Error fetching news: {e}")
             return []
 
-
     def fetch_sources(self):
+        """Function fetch_sources printing python version."""
         url = f"{self.base_url}/sources"
         params = {"apiKey": self.api_key}
-        
+
         try:
             response = requests.get(url, params=params)
             response.raise_for_status()
@@ -73,18 +92,19 @@ class NewsService:
                 {"name": "Source 2", "description": "Description 2"},
             ]
 
+
 def news_list_view(request):
-    form = CategoryForm(request.GET)  # Получаем данные формы из GET-запроса
+    """Function news_list_view printing python version."""
+    form = CategoryForm(request.GET)
     news = []
 
     if form.is_valid():
-        selected_category = form.cleaned_data.get('category')
+        selected_category = form.cleaned_data.get("category")
         if selected_category:
-            news_service = NewsService(api_key='your_api_key')
-            news = news_service.fetch_news(category=selected_category)  # Получаем новости по выбранной категории
+            news_service = NewsService(api_key="your_api_key")
+            news = news_service.fetch_news(category=selected_category)
     else:
-        # Если форма не отправлена, получаем все новости
-        news = NewsSummary.objects.all().order_by('-date')  # Получаем все новости, отсортированные по дате
 
-    return render(request, 'news/news_list.html', {'news': news, 'form': form})
+        news = NewsSummary.objects.all().order_by("-date")
 
+    return render(request, "news/news_list.html", {"news": news, "form": form})
