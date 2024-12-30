e9196cafa0370d357980d0f8d2300362666fa6b3
Nik Kor <3357381Nk@gmail.com>
2024-12-30 14:31:44 +0000

    Added Tag model, and field tags to Note model(many - many)

--- personal_assistant/notes/models.py
+++ personal_assistant/notes/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
-
-class Notes(models.Model):
+#classes for notes and tags. Each note might have multiple tags
+class Note(models.Model):
     user_id = models.IntegerField()
     title = models.CharField(max_length=100)
     content = models.TextField()
     done = models.BooleanField(default=False)
-
+    tags = models.ManyToManyField('Tag', blank=True)
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
