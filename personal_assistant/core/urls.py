4da809c622952dcb2cbbe4b4de10d0c6bdc4d6fc
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-31 09:38:20 +0200

    Update urls.py
    Главная страница index

--- personal_assistant/core/urls.py
+++ personal_assistant/core/urls.py
@@ -20,7 +20,7 @@ from news import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.news_list, name='index'),  # главная страница, отображает новости
+    path('', views.index, name='index'),  # главная страница, отображает новости
     path('news/', views.news_list, name='news_list'),  # страница с новостями
     path('news/sources/', views.news_sources, name='news_sources'),  # страница с источниками новостей
     path('news/weather/', views.weather_widget_view, name='weather_widget'),  # страница с погодой
