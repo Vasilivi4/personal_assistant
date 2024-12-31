39222d4856ca765c239a3b35874b1b6e68ad7adb
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-31 07:27:33 +0200

    Update urls.py

--- personal_assistant/accounts/urls.py
+++ personal_assistant/accounts/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from .views import signupuser, loginuser, logoutuser
+"""Module providing a function printing python version."""
 
-app_name = 'accounts'
+from django.urls import path
+from accounts.views import signupuser, loginuser, logoutuser
+
+app_name = "accounts"
 
 urlpatterns = [
-    path('signup/', signupuser, name='signup'),
-    path('login/', loginuser, name='login'),
-    path('logout/', logoutuser, name='logout'),
+    path("signup/", signupuser, name="signup"),
+    path("login/", loginuser, name="login"),
+    path("logout/", logoutuser, name="logout"),
     # path('profile/', profile, name='profile'),
-    ]
+]
