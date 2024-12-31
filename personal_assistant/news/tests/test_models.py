7dd97a7270cefa0a850ce622948ec98605d33e50
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-31 15:31:59 +0200

    Delete test_models.py

--- personal_assistant/news/tests/test_models.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from news.models import News
-
-@pytest.mark.django_db
-def test_news_model():
-    news = News.objects.create(title="Test Title", content="Test Content", category="finance")
-    assert news.title == "Test Title"
-    assert news.content == "Test Content"
-    assert news.category == "finance"
