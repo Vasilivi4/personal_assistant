391467264c66c4d8c1f44362c12f839cdca505c4
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-02 17:36:36 +0200

    Update urls.py

--- personal_assistant/news/urls.py
+++ personal_assistant/news/urls.py
@@ -7,13 +7,9 @@ from news import views
 app_name = "news"
 
 urlpatterns = [
-    # path("", views.news_list, name="news_list"),
-    # path("news/", views.news_list, name="news_list"),  # страница с новостями
-    # path(
-    #     "news/sources/", views.news_sources, name="news_sources"
-    # ),  # страница с источниками новостей
-    # path(
-    #     "news/weather/", views.weather_widget_view, name="weather_widget"
-    # ),  # страница с погодой
-    # path("news/daily-summary/", views.daily_summary, name="daily-summary"),
+    path('', views.index, name='index'),
+    path('list/', views.news_list, name='news_list'),
+    path('news/sources/', views.news_sources, name='news_sources'),
+    path('news/weather/', views.weather_widget_view, name='weather_widget'),
+    path('news/daily-summary/', views.daily_summary, name='daily-summary'),
 ]
