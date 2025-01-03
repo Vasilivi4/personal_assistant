a5086dcfe1dc21b2863d67de1fd0796a5c139990
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-03 15:55:49 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -6,6 +6,7 @@ from news.models import News
 
 @pytest.mark.django_db
 def test_news_list_view(client):
+    """Function test_news_list_view printing python version."""
     News.objects.create(title="Test News", content="Test Content", category="finance")
 
     url = reverse("news:news_list")
@@ -16,6 +17,7 @@ def test_news_list_view(client):
 
 @pytest.mark.django_db
 def test_news_list_category_filter(client):
+    """Function test_news_list_category_filter printing python version."""
     News.objects.create(title="Finance News", content="Content", category="finance")
     News.objects.create(title="Sports News", content="Content", category="sports")
 
