1569e82ef3357103bb4d1eba6e4e759db2e7659b
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-03 15:55:44 +0200

    Update test_services.py

--- tests/test_services.py
+++ tests/test_services.py
@@ -5,6 +5,7 @@ from news.services import NewsService
 
 @pytest.mark.django_db(transaction=True)
 def test_news_service_fetch_news(mocker):
+    """Function test_news_service_fetch_news printing python version."""
     mock_response = {"articles": [{"title": "Test News", "content": "Test Content"}]}
     mocker.patch("news.services.NewsService.fetch_news", return_value=mock_response)
 
