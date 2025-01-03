be01740468cc6da2ae416acbff937a71186edccf
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-03 12:25:53 +0200

    Update servic.py

--- personal_assistant/news/servic.py
+++ personal_assistant/news/servic.py
@@ -1,15 +1,16 @@
-"""Module providing a function printing python version."""
+# """Module providing a function printing python version."""
 
-import requests
+# import requests
 
-class CurrencyServic:
-    """Class CurrencyServic representing a person"""
-    def __init__(self, api_url, api_key):
-        self.api_url = api_url
-        self.api_key = api_key
+# class CurrencyServic:
+#     """Class CurrencyServic representing a person"""
+#     def __init__(self, api_url, api_key):
+#         self.api_url = api_url
+#         self.api_key = api_key
 
-    def fetch_rates(self):
-        """Function fetch_rates printing python version."""
-        response = requests.get(f"{self.api_url}?apikey={self.api_key}")
-        response.raise_for_status()
-        return response.json().get("rates", {})
+#     def fetch_rates(self):
+#         """Function fetch_rates printing python version."""
+#         response = requests.get(f"{self.api_url}?apikey={self.api_key}")
+#         response.raise_for_status()
+#         return response.json().get("rates", {})
+# Этот код не рабочий
