2185cb6d4f0d3df105a94b3e2455d37dc2b6152e
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-03 10:23:14 +0200

    Update urls.py

--- personal_assistant/contacts/urls.py
+++ personal_assistant/contacts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from contacts import views
+
+app_name = 'contacts'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Головна сторінка
+    path('home-contact/', views.get_index, name='hom_contact'),  # Головна сторінка
     path('contact/create/', views.contact_create, name='contact_create'),
     path('contact/edit/<int:pk>/', views.contact_edit, name='contact_edit'),
     path('contact/delete/<int:pk>/', views.contact_delete, name='contact_delete'),
