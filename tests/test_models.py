f750d6f61053211e5d8e18d063d9d26283ab1744
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-03 13:55:26 +0200

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+"""Module providing a function printing python version."""
+
 import pytest
 from django.contrib.auth.models import User
 from news.models import NewsSummary, News
@@ -5,6 +7,7 @@ from news.models import NewsSummary, News
 
 @pytest.mark.django_db(transaction=True)
 def test_news_model():
+    """Function test_news_model printing python version."""
     news = News.objects.create(title="Test Title", content="Test Content", category="finance")
     assert news.title == "Test Title"
     assert news.content == "Test Content"
@@ -13,40 +16,37 @@ def test_news_model():
 
 @pytest.mark.django_db
 def test_news_summary_model():
-    # Создаем пользователя
+    """Function test_news_model printing python version."""
     user = User.objects.create(username="testuser")
     
-    # Создаем экземпляр модели NewsSummary
     news_summary = NewsSummary.objects.create(
         user=user,
         title="Summary Title",
         content="Summary Content",
     )
-    
-    # Проверяем поля
     assert news_summary.title == "Summary Title"
     assert news_summary.content == "Summary Content"
     assert news_summary.user == user
-
-    # Проверяем строковое представление
     assert str(news_summary) == "Summary Title"
 
 @pytest.mark.django_db
 def test_news_model_invalid_category():
+    """Function test_news_model printing python version."""
     with pytest.raises(ValueError):
         News.objects.create(title="Invalid Category", content="Test Content", category="invalid")
 
 @pytest.mark.django_db
 def test_news_model_blank_content():
+    """Function test_news_model printing python version."""
     news = News.objects.create(title="Test Title", content="", category="sports")
     assert news.content == ""
 
 @pytest.mark.django_db
 def test_news_filter_by_category():
+    """Function test_news_model printing python version."""
     News.objects.create(title="Finance News", content="Finance Content", category="finance")
     News.objects.create(title="Sports News", content="Sports Content", category="sports")
 
-    # Фильтрация по категории
     finance_news = News.objects.filter(category="finance")
     assert len(finance_news) == 1
     assert finance_news[0].title == "Finance News"
