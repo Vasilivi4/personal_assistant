31ce53fa5333d84a39d5d4daaacea23c0a0a0093
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-03 15:55:46 +0200

    Update test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 def test_news_list_view(client):
+    """Function test_news_list_view printing python version."""
     url = reverse("news:news_list")
     response = client.get(url, {"category": "finance"})
     assert response.status_code == 200
