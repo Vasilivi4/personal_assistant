8f53cbb19f607db15aaa2831c31c5cb1289fa5f4
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-04 23:26:56 +0200

    Update models.py

--- personal_assistant/files/models.py
+++ personal_assistant/files/models.py
@@ -1,9 +1,7 @@
-# files/models.py
-
 from django.db import models
 
-# Модель для зберігання інформації про файли користувачів
-class UserFile(models.Model):
+# Модель для хранения информации о загруженных файлах
+class UploadedFile(models.Model):
     CATEGORY_CHOICES = [
         ('image', 'Зображення'),
         ('document', 'Документ'),
@@ -11,17 +9,13 @@ class UserFile(models.Model):
         ('other', 'Інше'),
     ]
 
-class UploadedFile(models.Model):
-    file = models.FileField(upload_to='uploads/')  # Поле для файлу
+    file = models.FileField(upload_to='uploads/')  # Поле для загружаемого файла
     category = models.CharField(
-    max_length=10,
-    choices=UserFile.CATEGORY_CHOICES,
-    default='other'
-    )  
-    
-    # Категорія файлу
-    uploaded_at = models.DateTimeField(auto_now_add=True)  # Дата завантаження
-    file = models.FileField(upload_to='uploads/')
+        max_length=10,
+        choices=CATEGORY_CHOICES,
+        default='other'
+    )  # Категория файла
+    uploaded_at = models.DateTimeField(auto_now_add=True)  # Дата загрузки
 
     def __str__(self):
         return self.file.name
