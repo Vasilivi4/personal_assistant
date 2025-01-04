c6a18d89ab03d0a0b2797410da7f24fc82f91999
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-04 23:27:04 +0200

    Update urls.py

--- personal_assistant/files/urls.py
+++ personal_assistant/files/urls.py
@@ -1,14 +1,9 @@
-# files/urls.py
-from storages.backends.gcloud import GoogleCloudStorage
 from django.urls import path
-from . import views
+from files import views
+
+app_name = "files"
 
 urlpatterns = [
-    path('upload/', views.upload_file, name='upload_file'),  # Завантаження файлів
-    path('', views.file_list, name='file_list'),  # Список файлів
+    path("upload/", views.upload_file, name="upload_file"),
+    path("file_list/", views.file_list, name="file_list"),
 ]
-
-# URL-адрес для завантажених файлів
-storage = GoogleCloudStorage()
-file_url = storage.url('<file-path>')
-
