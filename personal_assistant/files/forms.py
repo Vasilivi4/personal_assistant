11092d0e54f0bad6da2af5d1e3bb2645f8bce2fa
Xeljndjhtw <165076532+Xeljndjhtw@users.noreply.github.com>
2025-01-05 20:30:51 +0100

    Update forms.py

--- personal_assistant/files/forms.py
+++ personal_assistant/files/forms.py
@@ -1,9 +1,9 @@
 # files/forms.py
 from django import forms
-from .models import UserFile
+from .models import UploadedFile
 
 # Форма для завантаження файлів
 class UploadFileForm(forms.ModelForm):
     class Meta:
-        model = UserFile
+        model = UploadedFile
         fields = ['file', 'category']  # Поля для файлу та категорії
