01a76fcef7fe5e94e80f30e3c3dde143809c1739
Xeljndjhtw <165076532+Xeljndjhtw@users.noreply.github.com>
2025-01-07 12:42:03 +0100

    Update urls.py

--- personal_assistant/files/urls.py
+++ personal_assistant/files/urls.py
@@ -2,6 +2,8 @@
 from storages.backends.gcloud import GoogleCloudStorage
 from django.urls import path
 from . import views
+from google.cloud import storage
+from django.conf import settings
 
 app_name = 'files'
 
@@ -15,3 +17,20 @@ urlpatterns = [
 storage = GoogleCloudStorage()
 file_url = storage.url('<file-path>')
 
+def upload_to_gcs(file, file_name):
+    """
+    Завантаження файлу на Google Cloud Storage
+    """
+    # Ініціалізація клієнта
+    client = storage.Client.from_service_account_json(settings.GOOGLE_APPLICATION_CREDENTIALS)
+    bucket = client.get_bucket(settings.GCS_BUCKET_NAME)
+
+    # Створення об'єкта в bucket
+    blob = bucket.blob(file_name)
+
+    # Завантаження файлу
+    blob.upload_from_file(file)
+
+    # Доступний URL
+    blob.make_public()
+    return blob.public_url
