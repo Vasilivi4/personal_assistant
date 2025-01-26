eb5c03f5f5699a6b31d151ed0a525afa52c7bd6d
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-01-26 14:03:05 +0200

    Update urls.py

--- personal_assistant/contacts/urls.py
+++ personal_assistant/contacts/urls.py
@@ -6,8 +6,8 @@ from contacts import views
 app_name = 'contacts'
 
 urlpatterns = [
-    path('contacts/', views.index, name='contacts'),  # Головна сторінка
-    path('contact/create/', views.contact_create, name='contact_create'),
-    path('contact/edit/<int:pk>/', views.contact_edit, name='contact_edit'),
-    path('contact/delete/<int:pk>/', views.contact_delete, name='contact_delete'),
+    path('', views.contacts_list, name='contacts_list'),
+    path('create/', views.contact_create, name='contact_create'),
+    path('<int:pk>/edit/', views.contact_edit, name='contact_edit'),
+    path('<int:pk>/delete/', views.contact_delete, name='contact_delete'),
 ]
