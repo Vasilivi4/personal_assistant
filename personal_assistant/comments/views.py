eb91b87796702d306f2b2b4491c0f5886b061179
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-01 22:18:57 +0200

    Update views.py

--- personal_assistant/comments/views.py
+++ personal_assistant/comments/views.py
@@ -1,1 +1,23 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from comments.forms import CommentForm
+from news.models import Post
 
+def comment_list(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = post.comments.all()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('comments:comment_list', post_id=post.id)
+    else:
+        form = CommentForm()
+
+    return render(request, 'comments/comment_list.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    })
