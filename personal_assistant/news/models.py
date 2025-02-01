b2129cb47b849174ff9a8bc2dc08297035a0b13b
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-01 23:49:52 +0200

    Update models.py

--- personal_assistant/news/models.py
+++ personal_assistant/news/models.py
@@ -44,6 +44,7 @@ class News(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return self.title
