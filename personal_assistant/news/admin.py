1efdd55d8fe2043924b2e30027f4ec2d91c7547b
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-01 23:49:48 +0200

    Update admin.py

--- personal_assistant/news/admin.py
+++ personal_assistant/news/admin.py
@@ -1,5 +1,12 @@
 """Module providing a function printing python version."""
 
 from django.contrib import admin
+from comments.models import Post  # Импорт модели Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'created_at']
+
+
+admin.site.register(Post, PostAdmin)
+
