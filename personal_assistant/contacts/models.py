8b1f065f5c7550a5aeb9684b5a3e151a957d0dcb
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:44 +0200

    Update models.py

--- personal_assistant/contacts/models.py
+++ personal_assistant/contacts/models.py
@@ -1,4 +1,5 @@
-"""Module providing a function printing python version."""
+"""This module contains the Contact class representing a person."""
+# Version: 2021.09.13
 
 from django.db import models
 from django.core.validators import RegexValidator, EmailValidator
@@ -7,18 +8,21 @@ from django.contrib.auth.models import User
 
 class Contact(models.Model):
     """Class Contact representing a person"""
+
     name = models.CharField(max_length=100)
     address = models.TextField(blank=True, null=True)
     phone = models.CharField(
         max_length=15,
-        validators=[
-            RegexValidator(r"^\+?1?\d{9,15}$", "Invalid phone number format")
-        ],
+        validators=[RegexValidator(r"^\+?1?\d{9,15}$", "Invalid phone number format")],
     )
     email = models.EmailField(validators=[EmailValidator("Invalid email format")])
-    birthday = models.DateField(blank=True, null=True,
-                                help_text="Enter the date in YYYY-MM-DD format, e.g., 2000-01-01")
+    birthday = models.DateField(
+        blank=True,
+        null=True,
+        help_text="Enter the date in YYYY-MM-DD format, e.g., 2000-01-01",
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    objects = models.Manager()
 
     def __str__(self):
         """__str__ returns <type 'str'>"""
