4f772d0f2130714202132e9a83ff86f4b6842fd6
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:50 +0200

    Update views.py

--- personal_assistant/contacts/views.py
+++ personal_assistant/contacts/views.py
@@ -1,4 +1,4 @@
-"""Module providing a function printing python version."""
+"""This module contains views for the contacts app."""
 
 from datetime import timedelta, date
 from contacts.models import Contact
@@ -6,20 +6,19 @@ from contacts.forms import ContactForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def contacts_list(request):
+    """Function contacts_list printing python version."""
     today = date.today()
 
-    # Default end date (7 days from today)
     default_days = 7
     end_date = today + timedelta(days=default_days)
 
-    # Get filter parameters
-    query = request.GET.get('query', '')
-    month_number = request.GET.get('number', '')
-    days_left = request.GET.get('days', '')
+    query = request.GET.get("query", "")
+    month_number = request.GET.get("number", "")
+    days_left = request.GET.get("days", "")
 
-    # Handle empty or invalid 'days' and 'number' inputs
     try:
         days_left = int(days_left) if days_left else default_days
     except ValueError:
@@ -30,50 +29,47 @@ def contacts_list(request):
     except ValueError:
         month_number = None
 
-    # Filter all contacts
     contacts = Contact.objects.all()
 
     if query:
         contacts = contacts.filter(name__icontains=query)
 
-    # Filter contacts by month
     if month_number is not None:
         contacts = contacts.filter(birthday__month=month_number)
 
-    # Filter contacts with upcoming birthdays
     start_date = today
     end_date = today + timedelta(days=days_left)
     birthday_contacts = Contact.objects.filter(
         birthday__month__gte=start_date.month,
         birthday__day__gte=start_date.day,
         birthday__month__lte=end_date.month,
-        birthday__day__lte=end_date.day
+        birthday__day__lte=end_date.day,
     )
 
-    # Render contacts to the template
     return render(
         request,
-        'contacts/contacts_list.html',
+        "contacts/contacts_list.html",
         {
-            'contacts': contacts,
-            'birthday_contacts': birthday_contacts,
-            'days_left': days_left,
-        }
+            "contacts": contacts,
+            "birthday_contacts": birthday_contacts,
+            "days_left": days_left,
+        },
     )
+
 
 @login_required
 def contact_create(request):
     """Function contact_create printing python version."""
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             contact = form.save(commit=False)
             contact.user = request.user
             contact.save()
-            return redirect('contacts:contacts_list')
+            return redirect("contacts:contacts_list")
     else:
         form = ContactForm()
-    return render(request, 'contacts/contact_form.html', {'form': form})
+    return render(request, "contacts/contact_form.html", {"form": form})
 
 
 @login_required
@@ -84,10 +80,11 @@ def contact_edit(request, pk):
         form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             form.save()
-            return redirect('contacts:contacts_list')
+            return redirect("contacts:contacts_list")
     else:
         form = ContactForm(instance=contact)
     return render(request, "contacts/contact_form.html", {"form": form})
+
 
 @login_required
 def contact_delete(request, pk):
