0e48ae097aed74d5749c51d2b9dc9d34ff93c66a
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:24 +0200

    Update models.py

--- personal_assistant/comments/models.py
+++ personal_assistant/comments/models.py
@@ -1,10 +1,14 @@
+"""Comment models."""
+
 from django.db import models
 from news.models import Post
 
 class Comment(models.Model):
+    """Class Comment representing a person."""
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.text[:50]
+        """__str__ returns <type 'str'>."""
+        return str(self.text)[:50]
