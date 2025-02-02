f7aecdf41025c3f3e4c1024ee8e949dcd510acca
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:29:46 +0200

    Update models.py

--- personal_assistant/notes/models.py
+++ personal_assistant/notes/models.py
@@ -1,15 +1,18 @@
-"""Module providing a function printing python version."""
+"""This module contains the models for the notes app."""
 
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Note(models.Model):
     """Class Note representing a person"""
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     done = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    objects = models.Manager()
 
     def __str__(self):
         """__str__ returns <type 'str'>"""
@@ -19,15 +22,19 @@ class Note(models.Model):
 
 class Tag(models.Model):
     """Class Tag representing a person"""
+
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    objects = models.Manager()
 
     class Meta:
         """Class Meta representing a person"""
+
         constraints = [
-            models.UniqueConstraint(fields=['user', 'name'], name='tag of username')
+            models.UniqueConstraint(fields=["user", "name"], name="tag of username")
         ]
+
     def __str__(self):
         """__str__ returns <type 'str'>"""
-        self.title = self.name
+        self.name = self.name
         return self.name
