1747993b84b7c1a11dae1773421292703bcc8b1e
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:19 +0200

    Update apps.py

--- personal_assistant/comments/apps.py
+++ personal_assistant/comments/apps.py
@@ -1,6 +1,9 @@
+"""Comments app configuration."""
+
 from django.apps import AppConfig
 
 
 class CommentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comments'
+    """Class CommentsConfig representing a person."""
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "comments"
