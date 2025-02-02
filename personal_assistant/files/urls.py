340c7691bbfdac79b71d964d1da3466a594d5283
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:29:14 +0200

    Update urls.py

--- personal_assistant/files/urls.py
+++ personal_assistant/files/urls.py
@@ -1,4 +1,4 @@
-"""Module providing a function printing python version."""
+"""This module contains the URL configuration for the files app."""
 
 from django.urls import path
 from files import views
@@ -6,6 +6,6 @@ from files import views
 app_name = "files"
 
 urlpatterns = [
-    path('files/', views.file_list, name='file_list'),
-    path('upload/', views.file_upload, name='file_upload'),
+    path("files/", views.file_list, name="file_list"),
+    path("upload/", views.file_upload, name="file_upload"),
 ]
