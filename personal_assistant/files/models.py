909a57c6a8a21915e30b9ed24b52e8ad0b36ccce
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:29:11 +0200

    Update models.py

--- personal_assistant/files/models.py
+++ personal_assistant/files/models.py
@@ -1,29 +1,30 @@
 """Module providing a function printing python version."""
 
 from django.db import models
-from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 CATEGORY_CHOICES = [
-    ('image', 'Image'),
-    ('document', 'Document'),
-    ('video', 'Video'),
-    ('other', 'Other'),
+    ("image", "Image"),
+    ("document", "Document"),
+    ("video", "Video"),
+    ("other", "Other"),
 ]
+
 
 class File(models.Model):
     """Class File representing a person"""
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    file = CloudinaryField('file')
+    file = CloudinaryField("file")
     category = models.CharField(
-        max_length=20,
-        choices=CATEGORY_CHOICES,
-        default='other'
+        max_length=20, choices=CATEGORY_CHOICES, default="other"
     )
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
     def __str__(self):
         """__str__ returns <type 'str'>"""
-        self.title = self.name
+        self.name = self.name
         return self.name
