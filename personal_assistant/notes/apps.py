7071ac07c1a48d1026055956dfe25268cb655e63
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:29:42 +0200

    Update apps.py

--- personal_assistant/notes/apps.py
+++ personal_assistant/notes/apps.py
@@ -1,8 +1,10 @@
-"""Module providing a function printing python version."""
+"""This module contains the class NotesConfig that represents a person."""
 
 from django.apps import AppConfig
 
+
 class NotesConfig(AppConfig):
     """Class NotesConfig representing a person"""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notes'
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "notes"
