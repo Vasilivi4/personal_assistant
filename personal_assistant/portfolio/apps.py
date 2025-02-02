12e81834129ec30110f2eaa4dd7df993bd3f6461
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:30:00 +0200

    Create apps.py

---
+++ personal_assistant/portfolio/apps.py
@@ -0,0 +1,8 @@
+"""This file contains the configuration of the portfolio app."""
+
+from django.apps import AppConfig
+
+
+class PortfolioConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "portfolio"
