c4771cd803916327d12029153c4983b8adf881a8
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:29:06 +0200

    Update apps.py

--- personal_assistant/files/apps.py
+++ personal_assistant/files/apps.py
@@ -1,9 +1,11 @@
-"""Module providing a function printing python version."""
+"""Module representing the configuration of the files app."""
+# Version: 1.0
 
 from django.apps import AppConfig
 
 
 class FilesConfig(AppConfig):
     """Class FilesConfig representing a person"""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'files'
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "files"
