ebcd5c215d524fb6c188ff29830cd49e3bf87871
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:29:29 +0200

    Update urls.py

--- personal_assistant/news/urls.py
+++ personal_assistant/news/urls.py
@@ -1,4 +1,6 @@
-"""Module providing a function printing python version."""
+"""	URL Configuration for the news app.	"""
+
+# Description: URL configuration for the news app.
 
 from django.urls import path
 from news import views
@@ -7,9 +9,9 @@ from news import views
 app_name = "news"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('news/list/', views.news_list, name='news_list'),
-    path('news/sources/', views.news_sources, name='news_sources'),
-    path('weather_widget/', views.weather_widget_view, name='weather_widget'),
-    path('contact/', views.photo_gallery, name='photo_gallery'),
+    path("", views.index, name="index"),
+    path("news/list/", views.news_list, name="news_list"),
+    path("news/sources/", views.news_sources, name="news_sources"),
+    path("weather_widget/", views.weather_widget_view, name="weather_widget"),
+    path("contact/", views.photo_gallery, name="photo_gallery"),
 ]
