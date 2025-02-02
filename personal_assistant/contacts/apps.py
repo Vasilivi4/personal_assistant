e6600ad58f34044b5b75e0a83a08e316bdd875f0
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:34 +0200

    Update apps.py

--- personal_assistant/contacts/apps.py
+++ personal_assistant/contacts/apps.py
@@ -1,9 +1,10 @@
-"""Module providing a function printing python version."""
+"""This module contains the ContactsConfig class representing a person."""
 
 from django.apps import AppConfig
 
 
 class ContactsConfig(AppConfig):
     """Class ContactsConfig representing a person"""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "contacts"
