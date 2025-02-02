4ce5287b5cdd3ed4d113690cec9baef2f301efc7
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 09:57:48 +0200

    Update views.py

--- personal_assistant/comments/views.py
+++ personal_assistant/comments/views.py
@@ -6,6 +6,14 @@ def comment_list(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = post.comments.all()
 
+    image_files = [
+        "https://res.cloudinary.com/dxcgfa3e2/image/upload/v1737404075/images/ftopxylji1twl9o4kfmh.png",
+        "https://res.cloudinary.com/dxcgfa3e2/image/upload/v1737404076/images/nwmmcd1xfsokpk1brhdl.png",
+        "https://res.cloudinary.com/dxcgfa3e2/image/upload/v1737404076/images/udb11ue7ytxwccrxq6mm.png",
+        "https://res.cloudinary.com/dxcgfa3e2/image/upload/v1737404077/images/k6ofawgnhty1mdsxusf7.png",
+        "https://res.cloudinary.com/dxcgfa3e2/image/upload/v1737404078/images/orabb09asjwykxqftade.png",
+    ]
+
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -20,4 +28,5 @@ def comment_list(request, post_id):
         'post': post,
         'comments': comments,
         'form': form,
+        'image_files': image_files,
     })
