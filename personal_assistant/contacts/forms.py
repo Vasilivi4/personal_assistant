92f23cc6142bde88dcd5dbf2288108796469550c
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:36 +0200

    Update forms.py

--- personal_assistant/contacts/forms.py
+++ personal_assistant/contacts/forms.py
@@ -1,4 +1,5 @@
-"""Module providing a function printing python version."""
+"""Module forms.py representing a person."""
+# Version: 2021.09.13
 
 from django import forms
 from contacts.models import Contact
@@ -6,31 +7,28 @@ from contacts.models import Contact
 
 class ContactForm(forms.ModelForm):
     """Class ContactForm representing a person"""
+
     class Meta:
         """Class Meta representing a person"""
+
         model = Contact
         fields = ["name", "address", "phone", "email", "birthday"]
 
-
 class ContactSearchForm(forms.Form):
     """Class ContactSearchForm representing a person"""
-    query = forms.CharField(max_length=100, required=False, label="Пошук")
-    number = forms.IntegerField(required=False, label='Month')
-    days = forms.IntegerField(required=False, label='Days')
-
-
-class ContactSearchForm(forms.Form):
     query = forms.CharField(required=False)
     number = forms.IntegerField(required=False, min_value=1, max_value=12)
     days = forms.IntegerField(required=False, min_value=0)
+
     class Meta:
         """Class Meta representing a person"""
+
         model = Contact
-        fields = ['name', 'address', 'phone', 'email', 'birthday']
+        fields = ["name", "address", "phone", "email", "birthday"]
 
     def clean_phone(self):
-        """Function clean_phone printing python version."""
-        phone = self.cleaned_data['phone']
-        if not phone.startswith('+'):
+        """clean_phone returns phone"""
+        phone = self.cleaned_data["phone"]
+        if not phone.startswith("+"):
             raise forms.ValidationError("Номер телефона должен начинаться с '+'")
         return phone
