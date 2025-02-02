12221d734588c59cd7b0261e47c760cf32501736
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:31 +0200

    Update admin.py

--- personal_assistant/contacts/admin.py
+++ personal_assistant/contacts/admin.py
@@ -1,12 +1,13 @@
-"""Module providing a function printing python version."""
+"""Admin configuration for the contacts app."""
+# Version: 1.0
 
 from django.contrib import admin
-from .models import Contact
+from contacts.models import Contact
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     """Class ContactAdmin representing a person"""
+
     list_display = ("name", "phone", "email", "birthday")
     search_fields = ("name", "email", "phone")
-
