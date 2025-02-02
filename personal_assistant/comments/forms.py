732ec3ed909b8883877868a3a978c901ebd3a053
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:21 +0200

    Update forms.py

--- personal_assistant/comments/forms.py
+++ personal_assistant/comments/forms.py
@@ -1,7 +1,11 @@
+"""This module contains the CommentForm class, which is a form for the Comment model."""
+
 from django import forms
-from .models import Comment
+from comments.models import Comment
 
 class CommentForm(forms.ModelForm):
+    """A form for the Comment model."""
     class Meta:
+        """A class representing the metadata of the CommentForm class."""
         model = Comment
         fields = ['text']
