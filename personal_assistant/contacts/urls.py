581ece454e63b32798dc59e46839c3b46531f2dd
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:28:47 +0200

    Update urls.py

--- personal_assistant/contacts/urls.py
+++ personal_assistant/contacts/urls.py
@@ -1,13 +1,13 @@
-"""Module providing a function printing python version."""
+"""URLs for the contacts app."""
 
 from django.urls import path
 from contacts import views
 
-app_name = 'contacts'
+app_name = "contacts"
 
 urlpatterns = [
-    path('', views.contacts_list, name='contacts_list'),
-    path('create/', views.contact_create, name='contact_create'),
-    path('<int:pk>/edit/', views.contact_edit, name='contact_edit'),
-    path('<int:pk>/delete/', views.contact_delete, name='contact_delete'),
+    path("", views.contacts_list, name="contacts_list"),
+    path("create/", views.contact_create, name="contact_create"),
+    path("<int:pk>/edit/", views.contact_edit, name="contact_edit"),
+    path("<int:pk>/delete/", views.contact_delete, name="contact_delete"),
 ]
