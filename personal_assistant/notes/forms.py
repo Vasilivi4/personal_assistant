facb87899fb7d927d6b4375b027bfbef8b255fa0
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2025-02-02 17:29:44 +0200

    Update forms.py

--- personal_assistant/notes/forms.py
+++ personal_assistant/notes/forms.py
@@ -1,4 +1,4 @@
-"""Module providing a function printing python version."""
+"""Module for forms in the notes app"""
 
 from django import forms
 from notes.models import Note, Tag
@@ -6,13 +6,15 @@ from notes.models import Note, Tag
 
 class NoteForm(forms.ModelForm):
     """Class NoteForm representing a person"""
+
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
+        self.user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
         if self.user:
-            self.fields['tags'].queryset = Tag.objects.filter(user=self.user)
+            self.fields["tags"].queryset = Tag.objects.filter(user=self.user)
 
     class Meta:
         """Class Meta representing a person"""
+
         model = Note
-        fields = ['title', 'content', 'tags']
+        fields = ["title", "content", "tags"]
